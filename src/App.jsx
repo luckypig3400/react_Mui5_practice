bbb22f04e891f7e082fb65580b3c81c30ff471e3
郵差 <twHenryYang@gmail.com>
2023-07-20 20:50:59 +0800

    feat: 使用Mui Button元件
    + https://youtu.be/fzxEECHnsvU?t=393
    + https://mui.com/material-ui/react-button/

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import { Button } from "@mui/material"
+
 function App() {
   return (
     <div>
-      Hello
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
     </div>
   )
 }
