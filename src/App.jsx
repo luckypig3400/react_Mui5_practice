c39303804632bec620adcae903b57252679d08d9
郵差 <twHenryYang@gmail.com>
2023-07-31 15:29:04 +0800

    update: 調整Button color與size

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,15 @@ function App() {
   return (
     <div>
       <Button variant="text">Text</Button>
+      <Button variant="contained" disabled>Disabled</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
+      <br />
+      <Button variant="contained" color="primary" size="large">Primary Color</Button>
+      <Button variant="contained" color="secondary" size="small">Secondary Color</Button>
+      <Button variant="contained" color="warning">Warning</Button>
+      <Button variant="outlined" color="info" >info</Button>
+
     </div>
   )
 }
