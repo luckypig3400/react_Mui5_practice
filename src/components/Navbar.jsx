6c1625b5d9f7d60c2c4f6469c39be57af5923308
郵差 <twHenryYang@gmail.com>
2023-08-04 14:04:51 +0800

    feat: 客製化Mui AppBar與使用Mui icons
    + https://youtu.be/fzxEECHnsvU?t=1830
    + https://mui.com/material-ui/material-icons/?theme=Sharp

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,24 @@
-import { AppBar, Toolbar } from '@mui/material'
+import styled from '@emotion/styled'
+import { AppBar, Toolbar, Typography } from '@mui/material'
 import React from 'react'
+import LuckyPigIcon from '@mui/icons-material/SavingsSharp';
+// Icons Reference:
+// https://mui.com/material-ui/material-icons/?query=pig&theme=Sharp&selected=SavingsSharp
+
+const StyledToolbar = styled(Toolbar)({
+  display: "flex",
+  justifyContent: "space-between",
+});
 
 export default function Navbar() {
   return (
     <AppBar position="sticky">
-      <Toolbar>
-        Navbar
-      </Toolbar>
+      <StyledToolbar>
+        <Typography variant='h6' sx={{ display: { xs: "none", sm: "block" } }}>
+          LuckyPig
+        </Typography>
+        <LuckyPigIcon sx={{ display: { xs: "block", sm: "none" } }} />
+      </StyledToolbar>
     </AppBar>
   )
 }
