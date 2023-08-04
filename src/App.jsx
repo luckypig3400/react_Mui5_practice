68b6d3e28119cbfa3f11b6016b7e2cd36bbe4426
郵差 <twHenryYang@gmail.com>
2023-08-04 11:23:14 +0800

    feat: 複製貼上Mui 設計好的AppBar元件(好方便~)
    + https://mui.com/material-ui/react-app-bar/#app-bar-with-a-primary-search-field

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import MuiCrashCoursePractice from "./MuiCrashCoursePractice"
+import Navbar from "./components/Navbar"
 import Sidebar from "./components/Sidebar"
 import Feed from "./components/Feed"
 import Rightbar from "./components/Rightbar"
@@ -8,6 +9,7 @@ import { Box, Stack } from "@mui/material"
 function App() {
   return (
     <Box>
+      <Navbar />
       <Stack
         direction="row"
         spacing={2}
