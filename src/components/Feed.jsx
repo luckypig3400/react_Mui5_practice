1e3fce7e3e081a4070ec4e3111cfe7383ccd8001
郵差 <twHenryYang@gmail.com>
2023-08-05 17:14:01 +0800

    feat: 使用Mui Card元件(多元件複合成為貼文風格的卡片)
    + https://mui.com/material-ui/react-card/#complex-interaction
    + https://youtu.be/fzxEECHnsvU?t=2787

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,13 +1,52 @@
-import { Box } from '@mui/material'
+import { Avatar, Box, Card, CardActions, CardContent, CardHeader, CardMedia, IconButton, Typography } from '@mui/material'
 import React from 'react'
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
 
 export default function Feed() {
   return (
-    <Box bgcolor="lightyellow"
+    <Box
       flex={55}
       p={2}
     >
-      Feed
+      <Card>
+        <CardHeader
+          avatar={
+            <Avatar sx={{ bgcolor: "red" }} aria-label="recipe">
+              R
+            </Avatar>
+          }
+          action={
+            <IconButton aria-label="settings">
+              <MoreVertIcon />
+            </IconButton>
+          }
+          title="Vite React + Mui is super powerful"
+          subheader="August 05, 2023"
+        />
+        <CardMedia
+          component="img"
+          height="194"
+          image="public/vite.svg"
+          alt="Vite User"
+        />
+        <CardContent>
+          <Typography variant="body2" color="text.secondary">
+            This impressive paella is a perfect party dish and a fun meal to cook
+            together with your guests. Add 1 cup of frozen peas along with the mussels,
+            if you like.
+          </Typography>
+        </CardContent>
+        <CardActions disableSpacing>
+          <IconButton aria-label="add to favorites">
+            <FavoriteIcon />
+          </IconButton>
+          <IconButton aria-label="share">
+            <ShareIcon />
+          </IconButton>
+        </CardActions>
+      </Card>
     </Box>
   )
 }
