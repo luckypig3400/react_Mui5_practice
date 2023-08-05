afa445257bf3cc5690e1a9285a34f64ac8823cf1
郵差 <twHenryYang@gmail.com>
2023-08-05 20:22:42 +0800

    feat: 使用Mui ImageList
    + https://youtu.be/fzxEECHnsvU?t=3197
    + https://mui.com/material-ui/react-image-list/#standard-image-list

--- src/components/Rightbar.jsx
+++ src/components/Rightbar.jsx
@@ -1,4 +1,4 @@
-import { Avatar, AvatarGroup, Box, Typography } from '@mui/material'
+import { Avatar, AvatarGroup, Box, ImageList, ImageListItem, Typography } from '@mui/material'
 import React from 'react'
 
 export default function Rightbar() {
@@ -12,12 +12,12 @@ export default function Rightbar() {
         }
       }}
     >
-      <Box position="fixed">
+      <Box position="fixed" width={300}>
         <Typography variant='h6' fontWeight={100}>
           Online Friends
         </Typography>
 
-        <AvatarGroup max={7}>
+        <AvatarGroup max={6}>
           <Avatar alt="LuckyMan" src="https://previews.123rf.com/images/oriartiste/oriartiste1812/oriartiste181200207/114268122-lucky-pig-chinese-new-year-the-year-of-the-pig-lucky-pig-from-the-lucky-cat-translation.jpg" />
           <Avatar alt="Anya Foger" src="https://cdn.myanimelist.net/r/200x268/images/characters/4/457933.jpg?s=6eb706b2687d6e3b01a7cb254b5ca3e3" />
           <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
@@ -26,7 +26,74 @@ export default function Rightbar() {
           <Avatar alt="Agnes Walker" src="/static/images/avatar/4.jpg" />
           <Avatar alt="Trevor Henderson" src="/static/images/avatar/5.jpg" />
         </AvatarGroup>
+
+        <Typography variant='h6' fontWeight={100} mt={2} mb={2}>
+          Latest Photos
+        </Typography>
+        <ImageList cols={3} rowHeight={100} gap={5}>
+          {itemData.map((item) => (
+            <ImageListItem key={item.img}>
+              <img
+                src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+                srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                alt={item.title}
+                loading="lazy"
+              />
+            </ImageListItem>
+          ))}
+        </ImageList>
       </Box>
     </Box>
   )
 }
+
+const itemData = [
+  {
+    img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
+    title: 'Breakfast',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
+    title: 'Burger',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
+    title: 'Camera',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
+    title: 'Coffee',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
+    title: 'Hats',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
+    title: 'Honey',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
+    title: 'Basketball',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
+    title: 'Fern',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
+    title: 'Mushrooms',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
+    title: 'Tomato basil',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
+    title: 'Sea star',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
+    title: 'Bike',
+  },
+];
