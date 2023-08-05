9ff86a1c89cf5cf537388b06d4e26db0ad807601
郵差 <twHenryYang@gmail.com>
2023-08-05 21:37:30 +0800

    update: 調整Fab(Floating Action Button)按鈕位置為固定
    + https://mui.com/material-ui/react-floating-action-button/#basic-fab

--- src/components/AddPost.jsx
+++ src/components/AddPost.jsx
@@ -5,7 +5,13 @@ import AddIcon from '@mui/icons-material/Add';
 export default function AddPost() {
   return (
     <>
-      <Tooltip title="Add New Post">
+      <Tooltip title="Add New Post"
+        sx={{
+          position: "fixed",
+          bottom: 20,
+          left: { xs: "calc(50% - 25px)", md: 30 }
+        }}
+      >
         <Fab color="primary" aria-label="add">
           <AddIcon />
         </Fab>
