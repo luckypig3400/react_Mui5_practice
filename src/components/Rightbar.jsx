8c078d068eb96c041ad9246b4da98a702c4c4a39
郵差 <twHenryYang@gmail.com>
2023-08-05 20:00:20 +0800

    feat: 使用Mui AvatarGroup建立聯絡人區塊
    + https://youtu.be/fzxEECHnsvU?t=3138
    + https://mui.com/material-ui/react-avatar/#grouped

--- src/components/Rightbar.jsx
+++ src/components/Rightbar.jsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from '@mui/material'
+import { Avatar, AvatarGroup, Box, Typography } from '@mui/material'
 import React from 'react'
 
 export default function Rightbar() {
@@ -16,6 +16,16 @@ export default function Rightbar() {
         <Typography variant='h6' fontWeight={100}>
           Online Friends
         </Typography>
+
+        <AvatarGroup max={7}>
+          <Avatar alt="LuckyMan" src="https://previews.123rf.com/images/oriartiste/oriartiste1812/oriartiste181200207/114268122-lucky-pig-chinese-new-year-the-year-of-the-pig-lucky-pig-from-the-lucky-cat-translation.jpg" />
+          <Avatar alt="Anya Foger" src="https://cdn.myanimelist.net/r/200x268/images/characters/4/457933.jpg?s=6eb706b2687d6e3b01a7cb254b5ca3e3" />
+          <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+          <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
+          <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
+          <Avatar alt="Agnes Walker" src="/static/images/avatar/4.jpg" />
+          <Avatar alt="Trevor Henderson" src="/static/images/avatar/5.jpg" />
+        </AvatarGroup>
       </Box>
     </Box>
   )
