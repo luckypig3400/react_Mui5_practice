cc72c9f4b3f86f107d97ca467e6dbd33147f5dbb
郵差 <twHenryYang@gmail.com>
2023-08-06 13:02:38 +0800

    feat: Dark Theme!!! (使用Mui ThemeProvider)
    + https://youtu.be/fzxEECHnsvU?t=4151

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,37 @@
-import React from "react"
+import React, { useState } from "react"
 import MuiCrashCoursePractice from "./MuiCrashCoursePractice"
 import Navbar from "./components/Navbar"
 import Sidebar from "./components/Sidebar"
 import Feed from "./components/Feed"
 import Rightbar from "./components/Rightbar"
-import { Box, Stack } from "@mui/material"
+import { Box, Stack, ThemeProvider, createTheme } from "@mui/material"
 import AddPost from "./components/AddPost"
 
 function App() {
-  return (
-    <Box>
-      <Navbar />
-      <Stack
-        direction="row"
-        spacing={2}
-        justifyContent="space-between"
-      >
-        <Sidebar />
-        <Feed />
-        <Rightbar />
-      </Stack>
+  const [themeMode, setThemeMode] = useState("dark");
+  const darkTheme = createTheme({
+    palette: {
+      mode: themeMode,
+    }
+  });
 
-      <AddPost />
-    </Box>
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <Box>
+        <Navbar />
+        <Stack
+          direction="row"
+          spacing={2}
+          justifyContent="space-between"
+        >
+          <Sidebar />
+          <Feed />
+          <Rightbar />
+        </Stack>
+
+        <AddPost />
+      </Box>
+    </ThemeProvider>
   )
 }
 
