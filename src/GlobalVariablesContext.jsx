3fa3afe9883dd96aa58747befd7c1a8194a31309
郵差 <twHenryYang@gmail.com>
2023-08-14 14:47:21 +0800

    feat: 存放與使用多組全域變數(global States, useContext() )

--- src/GlobalVariablesContext.jsx
+++ src/GlobalVariablesContext.jsx
@@ -3,23 +3,53 @@ import { createContext, useContext, useState } from 'react';
 const GlobalVariablesContext = createContext();
 
 export function GlobalVariablesProvider({ children }) {
-  const [count, setCount] = useState(0);
+  const [counter1, setCounter1] = useState(0);
+  const [counter2, setCounter2] = useState(0);
+  const [counter3, setCounter3] = useState(0);
 
-  const increment = () => {
-    setCount(prevCount => prevCount + 1);
+  const incrementCounter1 = () => {
+    setCounter1(prevCounter => prevCounter + 1);
   };
 
-  const decrement = () => {
-    setCount(prevCount => prevCount - 1);
+  const decrementCounter1 = () => {
+    setCounter1(prevCounter => prevCounter - 1);
+  };
+
+  const incrementCounter2 = () => {
+    setCounter2(prevCounter => prevCounter + 1);
+  };
+
+  const decrementCounter2 = () => {
+    setCounter2(prevCounter => prevCounter - 1);
+  };
+
+  const incrementCounter3 = () => {
+    setCounter3(prevCounter => prevCounter + 1);
+  };
+
+  const decrementCounter3 = () => {
+    setCounter3(prevCounter => prevCounter - 1);
   };
 
   return (
-    <GlobalVariablesContext.Provider value={{ count, increment, decrement }}>
+    <GlobalVariablesContext.Provider
+      value={{
+        counter1,
+        incrementCounter1,
+        decrementCounter1,
+        counter2,
+        incrementCounter2,
+        decrementCounter2,
+        counter3,
+        incrementCounter3,
+        decrementCounter3,
+      }}
+    >
       {children}
     </GlobalVariablesContext.Provider>
   );
 }
 
-export function useCounter() {
+export function useGlobalVariables() {
   return useContext(GlobalVariablesContext);
 }
--- src/pages/Page3.jsx
+++ src/pages/Page3.jsx
@@ -1,16 +1,34 @@
 // Page3.js
 import React from 'react';
-import { useCounter } from '../GlobalVariablesContext';
+import { useGlobalVariables } from '../GlobalVariablesContext';
 
 function Page3() {
-  const { count, increment, decrement } = useCounter();
+  const {
+    counter1,
+    incrementCounter1,
+    decrementCounter1,
+    counter2,
+    incrementCounter2,
+    decrementCounter2,
+    counter3,
+    incrementCounter3,
+    decrementCounter3,
+  } = useGlobalVariables();
 
   return (
     <div>
       <h2>Page 3</h2>
-      <p>Count: {count}</p>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
+      <p>Counter 1: {counter1}</p>
+      <button onClick={incrementCounter1}>Increment Counter 1</button>
+      <button onClick={decrementCounter1}>Decrement Counter 1</button>
+
+      <p>Counter 2: {counter2}</p>
+      <button onClick={incrementCounter2}>Increment Counter 2</button>
+      <button onClick={decrementCounter2}>Decrement Counter 2</button>
+
+      <p>Counter 3: {counter3}</p>
+      <button onClick={incrementCounter3}>Increment Counter 3</button>
+      <button onClick={decrementCounter3}>Decrement Counter 3</button>
     </div>
   );
 }
