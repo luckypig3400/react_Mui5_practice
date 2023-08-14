26445f20ad634898ac6634e0f1cc2972d42c8aa5
郵差 <twHenryYang@gmail.com>
2023-08-14 17:35:03 +0800

    feat: 使用BrowserRouter來切換頁面

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import { GlobalVariablesProvider } from "./GlobalVariablesContext"
 import Page1 from "./pages/Page1"
 import Page2 from "./pages/Page2"
 import Page3 from "./pages/Page3"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 
 function App() {
   const [themeMode, setThemeMode] = useState("light");
@@ -22,28 +23,33 @@ function App() {
   return (
     <ThemeProvider theme={darkTheme}>
       <GlobalVariablesProvider>
-        <Page1 />
-        <Page2 />
-        <Page3 />
-      </GlobalVariablesProvider>
-
-      <Box
-        bgcolor={"background.default"}
-        color={"text.primary"}
-      >
-        <Navbar setMode={setThemeMode} mode={themeMode} />
-        <Stack
-          direction="row"
-          spacing={2}
-          justifyContent="space-between"
+        <Box
+          bgcolor={"background.default"}
+          color={"text.primary"}
         >
-          <Sidebar setMode={setThemeMode} mode={themeMode} />
-          <Feed />
-          <Rightbar />
-        </Stack>
+          <Navbar setMode={setThemeMode} mode={themeMode} />
+          <Stack
+            direction="row"
+            spacing={2}
+            justifyContent="space-between"
+          >
+            <Sidebar setMode={setThemeMode} mode={themeMode} />
 
-        <AddPost />
-      </Box>
+            <BrowserRouter>
+              <Routes>
+                <Route path="/" element={<Feed />} />
+                <Route path="page1" element={<Page1 />} />
+                <Route path="page2" element={<Page2 />} />
+                <Route path="page3" element={<Page3 />} />
+              </Routes>
+            </BrowserRouter>
+
+            <Rightbar />
+          </Stack>
+
+          <AddPost />
+        </Box>
+      </GlobalVariablesProvider>
     </ThemeProvider>
   )
 }
